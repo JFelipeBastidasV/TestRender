5ac6d9bc19067f8517838bcf02b0af6929d6dfeb
TulKasTer <23609872+TulKasTer@users.noreply.github.com>
2025-11-11 15:51:51 -0700

    feat(master): Create fetPostById, updatePost, deletePost

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -29,3 +29,62 @@ export const getAllPosts = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 }
+
+export const getPostById = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const selectPostQuery = `
+            SELECT id, content, created_at
+            FROM posts
+            WHERE id = $1`;
+        const result = await query(selectPostQuery, [id]);
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: 'Post not found' });
+        }
+
+        res.json(result.rows[0]);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export const updatePost = async (req, res) => {
+    const { id } = req.params;
+    const { content } = req.body;
+
+    try {
+        const updatePostQuery = `
+            UPDATE posts
+            SET content = $1, updated_at = CURRENT_TIMESTAMP
+            WHERE id = $2
+            RETURNING id, content, updated_at`;
+
+        const result = await query(updatePostQuery, [content, id]);
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: 'Post not found' });
+        }
+
+        res.json(result.rows[0]);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deletePostQuery = `
+            DELETE FROM posts
+            WHERE id = $1
+            RETURNING id`;
+        const result = await query(deletePostQuery, [id]);
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: 'Post not found' });
+        }
+        res.json({ message: 'Post deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -8,5 +8,8 @@ const router = express.Router();
 
 router.post('/', createPost);
 router.get('/', getAllPosts);
+router.get('/:id', getPostById);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
 
 export default router;
